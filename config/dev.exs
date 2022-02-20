51ff0d68759f832aaa8e6941447abb40812ad6d2
Patrick McDonnell <patrick@golocate.uk>
2022-02-20 21:16:13 +0000

    initial landing page

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :consider, ConsiderWeb.Endpoint,
   secret_key_base: "1Il7rliFlQM+W2HPLOlyOIRUwn5rsx7xyVO4MEzOCK5WMe7vYDFfSVhgfE5o/QY3",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
--- test/consider_web/controllers/page_controller_test.exs
+++ test/consider_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ConsiderWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Consider Elixir"
   end
 end
